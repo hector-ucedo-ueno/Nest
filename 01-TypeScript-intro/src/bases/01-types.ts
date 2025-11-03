dc897ef435c240caa434f4dd7206991490e35ef1
Héctor Ucedo <hector.ucedo@itti.digital>
2025-11-03 11:03:33 -0300

    12. Tipo de datos - continuacion

--- 01-TypeScript-intro/src/bases/01-types.ts
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -1,7 +1,16 @@
 
 
-export const name = 'Héctor';
+export let name = 'Héctor';
 export const age: number = 33;
 export const isValid: boolean = true;
 
-console.log(name, age, isValid);
+//name = 'Juan'; //? Cannot assign to 'name' because it is a constant.ts
+//name = true; //? Cannot assign to 'name' because it is a constant.ts
+name = 'Joy'; 
+
+export const templateString = ` Este es un template string con:
+nombre ${name},
+edad ${age} y,
+estado ${isValid}`;
+
+console.log(templateString);
--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import { name } from './bases/01-types.ts'
+import { name, age } from './bases/01-types.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
