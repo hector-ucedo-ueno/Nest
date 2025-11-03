e211115f321bb1e31be609ac0a0d4a0bf01673ea
Héctor Ucedo <hector.ucedo@itti.digital>
2025-11-03 11:56:58 -0300

    16. Gettters, metodos y THIS

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -1,17 +1,28 @@
 
-
-
+//? Versión larga de la clase
 export class Pokemon {
 
-    public id: number;
-    public name: string;
-
-    constructor(id: number, name: string) {
-        this.id = id;
-        this.name = name;
-        console.log('constructor llamado');
+    get imageUrl(): string {
+        return `https://pokeapi.co/api/v2/pokemon/${this.id}/`;
     }
 
+    constructor(
+        public readonly id: number,
+        public name: string,
+        // public imageUrl: string
+    ) {}
+
+    public scream() {
+        console.log(`${this.name.toUpperCase()} está gritando!`);
+    }
+
+    //private speak() {
+    speak() {
+        console.log(`${this.name} ${this.name} está hablando!`);
+    }
 }
 
 export const charmander = new Pokemon(1, 'Charmander')
+
+charmander.scream();
+charmander.speak(); //?Property 'speak' is private and only accessible within class 'Pokemon'.
